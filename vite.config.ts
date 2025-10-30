df07ef42a56ddfd3ea0112422f51ca3ee7787ec5
joshuaJrodrigues <jrodrigues@teknorix.com>
2025-10-30 16:28:34 +0530

    wrangler stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart(),
     viteReact(),
   ],
 })
-
-export default config
